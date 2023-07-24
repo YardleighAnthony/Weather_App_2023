cf91eb707abdafd31c53ccd924b4444dd42fac8c
Yardleigh Anthony <yardleighanthony@gmail.com>
2023-07-24 21:44:44 +0200

    new code

--- script.js
+++ script.js
@@ -41,95 +41,68 @@ setInterval(() => {
     dateElement.innerHTML = days[day] + ', ' + months[month] + ' ' + date;
 }, 1000);
 
-
-
-
-
 async function getWeatherData(city) {
+    try {
+        const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
+        const data = await response.json();
 
-    const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
-    const data = await response.json();
+        console.log(data);
 
-    console.log(data);
+        document.querySelector(".time-zone").innerHTML = data.name;
+        document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".time-zone").innerHTML = data.name;
+        document.querySelector(".Humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        document.querySelector(".pressure").innerHTML = data.main.pressure + " Pa";
+        document.querySelector(".sunrise").innerHTML = formatTime(data.sys.sunrise);
+        document.querySelector(".sunset").innerHTML = formatTime(data.sys.sunset);
 
-    //Select all elements and update the data
-    document.querySelector(".time-zone").innerHTML = data.name;
-    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".time-zone").innerHTML = data.name;
-    document.querySelector(".Humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-    document.querySelector(".pressure").innerHTML = data.main.pressure + " Pa";
-    document.querySelector(".sunrise").innerHTML = formatTime(data.sys.sunrise);
-    document.querySelector(".sunset").innerHTML = formatTime(data.sys.sunset);
-
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png";
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "]Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
     }
-    else if(data.weather[0].main == "Clear") {
-        weatherIcon.src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "]Mist"){
-        weatherIcon.src = "images/mist.png";
-    }
-    else if(data.weather[0].main == "Snow"){
-        weatherIcon.src = "images/snow.png";
-    }
-
 }
-// Add event listener to the button
+
 searchBtn.addEventListener("click", () => {
-    getWeatherData(searchInput.value); 
+    getWeatherData(searchInput.value);
 });
 
-//Formatting the time for sunset and sunrise
 function formatTime(unixTimestamp) {
     const date = new Date(unixTimestamp * 1000);
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const ampm = hours >= 12 ? 'PM' : 'AM';
-  
+
     const formattedHours = (hours % 12).toString().padStart(2, '0');
     const formattedMinutes = minutes.toString().padStart(2, '0');
-  
+
     return `${formattedHours}:${formattedMinutes} ${ampm}`;
-  }
-
-weatherForcast();
-
-function weatherForcast(){
-    navigator.geolocation.getCurrentPosition((success) =>{
-        let{latitude,longitude} = success.coords;
-
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`)
-        .then(response => response.json()).then(data => {
-            console.log(data);
-        })
-    })
 }
-    
 
-   /* navigator.geolocation.getCurrentPosition((success) => {
+weatherForecast();
 
-        let { latitude, longitude } = success.coords;
+function weatherForecast() {
+    navigator.geolocation.getCurrentPosition(async (success) => {
+        const { latitude, longitude } = success.coords;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`);
+        const dataWeekly = await response.json();
+        console.log(dataWeekly);
+        displayWeeklyForecast(dataWeekly);
+    });
+}
 
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-            }) 
-            .catch(error => {
-                console.error('Error fetching weather data:', error);
-            });
-    }, (error) => {
-        console.error('Error getting geolocation:', error);
-    });*/
 
